437e543c2c32147995bb079e546bc7e3844a45a5
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-20 16:00:52 +0100

    :sparkles: (demo) import the vite starter in demo/ folder

---
+++ demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
