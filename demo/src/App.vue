c3b86f046f7460e92a6f9199b1cf4e996b64af92
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-21 11:30:25 +0100

    :fire: (app) update App.vue

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -5,14 +5,3 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
