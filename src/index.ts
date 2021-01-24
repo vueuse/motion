1be78a8748abe864022116b29a5c18d04f823ae5
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-24 16:11:40 +0100

    :fire: (index) index exports composables

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
-import { ref } from 'vue-demi'
-
-/**
- * A simple toggler.
- *
- * @param [defaultValue=false]
- */
-const useToggle = (defaultValue: boolean = false) => {
-  const toggleable = ref(defaultValue)
-
-  const toggle = () => (toggleable.value = !toggleable.value)
-
-  return [toggleable, toggle] as const
-}
-
-export default useToggle
+export { useStyle } from './useStyle'
+export { useTransform } from './useTransform'
