a246a4db68545f7865537bbd815aae2d99aaa69a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-24 16:09:55 +0100

    :label: (types) add types file and import transform properties

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface TransformProperties {
+  x?: string | number
+  y?: string | number
+  z?: string | number
+  translateX?: string | number
+  translateY?: string | number
+  translateZ?: string | number
+  rotate?: string | number
+  rotateX?: string | number
+  rotateY?: string | number
+  rotateZ?: string | number
+  scale?: string | number
+  scaleX?: string | number
+  scaleY?: string | number
+  scaleZ?: string | number
+  skew?: string | number
+  skewX?: string | number
+  skewY?: string | number
+  originX?: string | number
+  originY?: string | number
+  originZ?: string | number
+  perspective?: string | number
+  transformPerspective?: string | number
+}
