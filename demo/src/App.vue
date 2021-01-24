f4e2edcd7715209177b7bce42b1499df787303af
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-24 20:38:12 +0100

    :sparkles: (demo) basic demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    <div>Hello</div>
+  <div :style="{ padding: '100px' }">
+    <div ref="popRef" :style="{ position: 'relative', backgroundColor: 'red' }">
+      Hello
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { useStyle } from '@lib'
+import { animate } from 'popmotion'
+import { onMounted, ref } from 'vue'
 
-export default defineComponent({
-  name: 'App',
+const popRef = ref<HTMLElement>()
+
+const { style } = useStyle(popRef)
+
+onMounted(() => {
+  animate({
+    from: -100,
+    to: 0,
+    type: 'spring',
+    onUpdate: (latest) => (style.bottom = `${latest}px`),
+  })
+
+  animate({
+    from: 0,
+    to: 1,
+    onUpdate: (latest) => (style.opacity = `${latest}`),
+  })
 })
 </script>
