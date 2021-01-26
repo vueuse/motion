881a6dbd9988d1b85e51a6020fd0eb8d3560ee19
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-26 17:35:33 +0100

    :label: (typings) improve style properties typings

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,68 @@
-import { Properties as CSSTypes } from 'csstype'
 import { SpringOptions } from 'popmotion'
 
-export interface CSSProperties
-  extends Omit<CSSTypes<string | number>, 'rotate' | 'scale'> {}
+export interface PropertiesKeys {
+  [key: string]: string | number | undefined | any
+}
 
-export interface TransformProperties {
+export interface StyleProperties extends PropertiesKeys {
+  // Color props
+  color?: string | number
+  backgroundColor?: string | number
+  outlineColor?: string | number
+  fill?: string | number
+  stroke?: string | number
+
+  // Border props
+  borderColor?: string | number
+  borderTopColor?: string | number
+  borderRightColor?: string | number
+  borderBottomColor?: string | number
+  borderLeftColor?: string | number
+  borderWidth?: string | number
+  borderTopWidth?: string | number
+  borderRightWidth?: string | number
+  borderBottomWidth?: string | number
+  borderLeftWidth?: string | number
+  borderRadius?: string | number
+  radius?: string | number
+  borderTopLeftRadius?: string | number
+  borderTopRightRadius?: string | number
+  borderBottomRightRadius?: string | number
+  borderBottomLeftRadius?: string | number
+
+  // Positioning props
+  width?: string | number
+  maxWidth?: string | number
+  height?: string | number
+  maxHeight?: string | number
+  size?: string | number
+  top?: string | number
+  right?: string | number
+  bottom?: string | number
+  left?: string | number
+
+  // Spacing props
+  padding?: string | number
+  paddingTop?: string | number
+  paddingRight?: string | number
+  paddingBottom?: string | number
+  paddingLeft?: string | number
+  margin?: string | number
+  marginTop?: string | number
+  marginRight?: string | number
+  marginBottom?: string | number
+  marginLeft?: string | number
+
+  // Misc
+  zIndex?: string | number
+
+  // SVG
+  fillOpacity?: string | number
+  strokeOpacity?: string | number
+  numOctaves?: string | number
+}
+
+export interface TransformProperties extends PropertiesKeys {
   x?: string | number
   y?: string | number
   z?: string | number
@@ -29,14 +87,17 @@ export interface TransformProperties {
   transformPerspective?: string | number
 }
 
-export interface MotionProperties extends CSSProperties, TransformProperties {}
+export interface MotionProperties
+  extends StyleProperties,
+    TransformProperties {}
 
 export interface Variant extends MotionProperties {
   spring?: Omit<SpringOptions, 'from' | 'to' | 'transition'>
 }
 
-export type VariantNames = 'initial' | 'enter' | 'exit'
-
 export type MotionVariants = {
-  [key: string]: Variant
+  initial?: Variant
+  enter?: Variant
+  leave?: Variant
+  [key: string]: Variant | undefined
 }
