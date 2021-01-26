131a2299d7acf6703354be6e15e08b7cc9c6e317
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-26 23:42:38 +0100

    :sparkles: (useStyle) improve useStyle, read base styles from element

--- src/useStyle.ts
+++ src/useStyle.ts
@@ -1,11 +1,29 @@
 import { MaybeRef } from '@vueuse/shared'
 import { ref, watch } from 'vue'
 import { reactiveStyle } from './reactiveStyle'
+import { valueTypes } from './utils/style'
 
 export const useStyle = (target: MaybeRef<HTMLElement | null | undefined>) => {
   const targetRef = ref(target)
 
   const { state, style } = reactiveStyle()
+
+  watch(
+    targetRef,
+    (newVal: HTMLElement | null | undefined) => {
+      if (!newVal || !newVal.style) return
+
+      for (const key of Object.keys(valueTypes)) {
+        // @ts-expect-error
+        if (newVal.style[key] === undefined) continue
+        // @ts-expect-error
+        if (!newVal.style[key]) state[key] = newVal.style[key]
+      }
+    },
+    {
+      immediate: true,
+    },
+  )
 
   watch(style, (newValue) => {
     if (!targetRef || !targetRef.value || !newValue) return
