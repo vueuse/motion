fbe68213c51dc3ed8d16508e873d4356be9e3e6e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-26 14:34:30 +0100

    :sparkles: (useStyle) update useStyle.. prepare for depreciation

--- src/useStyle.ts
+++ src/useStyle.ts
@@ -2,7 +2,9 @@ import { MaybeRef } from '@vueuse/shared'
 import { reactive, ref, watch } from 'vue'
 import { CSSProperties } from './types'
 
-const isValidStyleEntry = ([key, value]: string[]) =>
+type StyleEntryProps = [key: string, value: string | undefined | number]
+
+const isValidStyleEntry = ([key, value]: StyleEntryProps) =>
   // @ts-expect-error
   key && key !== '' && value && value !== '' && isNaN(key)
 
@@ -11,16 +13,22 @@ export const useStyle = (target: MaybeRef<HTMLElement | null | undefined>) => {
 
   const targetRef = ref(target)
 
-  watch(targetRef, (newValue) => {
-    if (!newValue) return
+  watch(
+    targetRef,
+    (newValue) => {
+      if (!newValue) return
 
-    Object.entries(newValue.style).forEach(([key, value]) => {
-      if (!isValidStyleEntry([key, value])) return
+      Object.entries(newValue.style).forEach(([key, value]) => {
+        if (!isValidStyleEntry([key, value])) return
 
-      // @ts-expect-error
-      style[key] = value
-    })
-  })
+        // @ts-expect-error
+        style[key] = value
+      })
+    },
+    {
+      immediate: true,
+    },
+  )
 
   watch(
     style,
@@ -28,6 +36,8 @@ export const useStyle = (target: MaybeRef<HTMLElement | null | undefined>) => {
       if (!targetRef || !targetRef.value || !targetRef.value.style) return
 
       for (const [key, value] of Object.entries(newValue)) {
+        if (!isValidStyleEntry([key, value])) return
+
         // @ts-expect-error
         targetRef.value.style[key] = value
       }
