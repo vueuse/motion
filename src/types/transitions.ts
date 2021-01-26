335893d173874785f2d4331b29211d6d37121a69
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-26 23:47:14 +0100

    :label: (transitions) improve transition typings

---
+++ src/types/transitions.ts
@@ -0,0 +1,18 @@
+import { AnimationOptions } from 'popmotion'
+
+export type TransitionProperties = Exclude<
+  AnimationOptions<number | number[]>,
+  'from' | 'to' | 'onComplete' | 'onPlay' | 'onRepeat' | 'onStop' | 'onUpdate'
+> & {
+  instant?: boolean
+}
+
+export type TransitionValues = {
+  from: any
+  to: any
+  onUpdate?: (latest: any) => void
+  onPlay?: () => void
+  onComplete?: () => void
+  onRepeat?: () => void
+  onStop?: () => void
+}
