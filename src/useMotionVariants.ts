ab50838f7f9d41a7e1caf5c4d358dad9c8bc10b6
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-26 17:47:14 +0100

    :sparkles: (useMotionVariants) improve motion variants handling

--- src/useMotionVariants.ts
+++ src/useMotionVariants.ts
@@ -1,28 +1,41 @@
+import { MaybeRef } from '@vueuse/shared'
 import { computed, onBeforeMount, onBeforeUnmount, onMounted, ref } from 'vue'
 import { MotionVariants, Variant } from './types'
 
-export const useMotionVariants = (variants: MotionVariants) => {
+export const useMotionVariants = (
+  variants: MaybeRef<MotionVariants | null | undefined> = {},
+) => {
+  const variantsRef = ref(variants)
+
   const currentVariantName = ref<string>('initial')
 
-  const currentVariant = computed(() => {
-    return variants[currentVariantName.value]
+  const currentVariant = computed<Variant | undefined>(() => {
+    if (
+      !variantsRef ||
+      !variantsRef.value ||
+      !variantsRef.value[currentVariantName.value]
+    )
+      return undefined
+
+    return variantsRef.value[currentVariantName.value]
   })
 
   const set = (variant: string) => {
-    let newVariant: Variant = variants[variant]
-
-    if (!newVariant) {
+    if (!variantsRef || !variantsRef.value || !variantsRef.value[variant]) {
       throw new Error(`The variant ${variant} cannot be found.`)
     }
 
     currentVariantName.value = variant
   }
 
-  if (variants.initial) onBeforeMount(() => set('initial'))
+  if (variantsRef && variantsRef.value && variantsRef.value.initial)
+    onBeforeMount(() => set('initial'))
 
-  if (variants.enter) onMounted(() => set('enter'))
+  if (variantsRef && variantsRef.value && variantsRef.value.enter)
+    onMounted(() => set('enter'))
 
-  if (variants.exit) onBeforeUnmount(() => set('exit'))
+  if (variantsRef && variantsRef.value && variantsRef.value.leave)
+    onBeforeUnmount(() => set('leave'))
 
   return {
     currentVariant,
