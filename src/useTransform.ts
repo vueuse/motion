3d55056e534c7b994435a68ea1efb19e493d9510
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-27 22:29:25 +0100

    :sparkles: (useTransform) cache transform string until the element is present, then sync

--- src/useTransform.ts
+++ src/useTransform.ts
@@ -5,14 +5,40 @@ import { reactiveTransform } from './reactiveTransform'
 export function useTransform(target: MaybeRef<HTMLElement | null | undefined>) {
   const targetRef = ref(target)
 
+  const _cache = ref<string>()
+
   const { state, transform } = reactiveTransform()
 
   // Sync reactive transform to element
-  watch(transform, (newValue) => {
-    if (!targetRef || !targetRef.value || !newValue) return
+  watch(
+    transform,
+    (newValue) => {
+      if (!targetRef || !targetRef.value) {
+        _cache.value = newValue
+        return
+      }
 
-    targetRef.value.style.transform = newValue
-  })
+      targetRef.value.style.transform = newValue
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  // Cache transform until the element is alive and we can bind to it
+  watch(
+    targetRef,
+    (newValue) => {
+      if (!newValue) return
+
+      if (_cache && _cache.value) {
+        newValue.style.transform = _cache.value
+      }
+    },
+    {
+      immediate: true,
+    },
+  )
 
   return {
     transform: state,
