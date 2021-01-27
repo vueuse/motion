bab3bb7269d9252ce7c78ac57a8080ffad35b76b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-27 19:26:45 +0100

    :label: (variants) improve variant css typings by importing dom defs from vue

--- src/types/variants.ts
+++ src/types/variants.ts
@@ -1,3 +1,4 @@
+import { CSSProperties } from 'vue'
 import { TransitionProperties } from './transitions'
 
 export interface PropertiesKeys {
@@ -8,64 +9,6 @@ export interface SVGPathProperties {
   pathLength?: number
   pathOffset?: number
   pathSpacing?: number
-}
-
-export interface StyleProperties extends PropertiesKeys {
-  // Color props
-  color?: string | number
-  backgroundColor?: string | number
-  outlineColor?: string | number
-  fill?: string | number
-  stroke?: string | number
-
-  // Border props
-  borderColor?: string | number
-  borderTopColor?: string | number
-  borderRightColor?: string | number
-  borderBottomColor?: string | number
-  borderLeftColor?: string | number
-  borderWidth?: string | number
-  borderTopWidth?: string | number
-  borderRightWidth?: string | number
-  borderBottomWidth?: string | number
-  borderLeftWidth?: string | number
-  borderRadius?: string | number
-  radius?: string | number
-  borderTopLeftRadius?: string | number
-  borderTopRightRadius?: string | number
-  borderBottomRightRadius?: string | number
-  borderBottomLeftRadius?: string | number
-
-  // Positioning props
-  width?: string | number
-  maxWidth?: string | number
-  height?: string | number
-  maxHeight?: string | number
-  size?: string | number
-  top?: string | number
-  right?: string | number
-  bottom?: string | number
-  left?: string | number
-
-  // Spacing props
-  padding?: string | number
-  paddingTop?: string | number
-  paddingRight?: string | number
-  paddingBottom?: string | number
-  paddingLeft?: string | number
-  margin?: string | number
-  marginTop?: string | number
-  marginRight?: string | number
-  marginBottom?: string | number
-  marginLeft?: string | number
-
-  // Misc
-  zIndex?: string | number
-
-  // SVG
-  fillOpacity?: string | number
-  strokeOpacity?: string | number
-  numOctaves?: string | number
 }
 
 export interface TransformProperties extends PropertiesKeys {
@@ -92,6 +35,9 @@ export interface TransformProperties extends PropertiesKeys {
   perspective?: string | number
   transformPerspective?: string | number
 }
+
+export interface StyleProperties
+  extends Omit<CSSProperties, keyof TransformProperties> {}
 
 export interface Variant extends StyleProperties, TransformProperties {
   transition?: TransitionProperties
