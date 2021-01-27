65e18686623fda50256ee358f00fd781686fb0f1
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-27 19:22:51 +0100

    :sparkles: (useMotionVariants) improved variant type inference, allowing user level autocomplete

--- src/useMotionVariants.ts
+++ src/useMotionVariants.ts
@@ -1,50 +1,39 @@
 import { MaybeRef } from '@vueuse/shared'
-import { computed, onBeforeMount, onBeforeUnmount, onMounted, ref } from 'vue'
+import {
+  computed,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  Ref,
+  ref,
+} from 'vue'
 import { MotionVariants, Variant } from './types/variants'
 
-export const useMotionVariants = (
-  variants: MaybeRef<MotionVariants | null | undefined> = {},
-) => {
-  const variantsRef = ref(variants)
+export function useMotionVariants<T extends MotionVariants>(
+  variants: MaybeRef<T> = {} as MaybeRef<T>,
+  initial: MaybeRef<keyof T> = 'initial',
+) {
+  const variantsRef = ref(variants) as Ref<T>
 
-  const currentVariantName = ref<string>()
+  const name = ref(initial) as Ref<keyof T>
 
-  const currentVariant = computed<Variant | undefined>(() => {
-    if (
-      !variantsRef ||
-      !variantsRef.value ||
-      !currentVariantName ||
-      !currentVariantName.value ||
-      !variantsRef.value[currentVariantName.value]
-    )
-      return undefined
+  const state = computed<Variant | undefined>(() => {
+    if (!name.value) return undefined
 
-    return variantsRef.value[currentVariantName.value]
+    return variantsRef.value[name.value]
   })
 
-  const set = (variant: string) => {
-    if (!variantsRef || !variantsRef.value || !variantsRef.value[variant]) {
-      throw new Error(`The variant ${variant} cannot be found.`)
-    }
-
-    currentVariantName.value = variant
-  }
-
   // Set initial before the element is mounted
-  if (variantsRef && variantsRef.value && variantsRef.value.initial)
-    onBeforeMount(() => set('initial'))
+  if (variantsRef.value.initial) onBeforeMount(() => (name.value = 'initial'))
 
   // Set enter animation, once the element is mounted
-  if (variantsRef && variantsRef.value && variantsRef.value.enter)
-    onMounted(() => set('enter'))
+  if (variantsRef.value.enter) onMounted(() => (name.value = 'enter'))
 
   // Set the leave animation, before the element is unmounted
-  if (variantsRef && variantsRef.value && variantsRef.value.leave)
-    onBeforeUnmount(() => set('leave'))
+  if (variantsRef.value.leave) onBeforeUnmount(() => (name.value = 'leave'))
 
   return {
-    currentVariant,
-    currentVariantName,
-    set,
+    state,
+    name,
   }
 }
