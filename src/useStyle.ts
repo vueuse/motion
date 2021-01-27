7c301d848772da1461bcbbdee105d1989008e9aa
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-27 15:37:32 +0100

    :sparkles: (useStyle) improve composable, sync existing element w/ state immediatly

--- src/useStyle.ts
+++ src/useStyle.ts
@@ -8,16 +8,21 @@ export const useStyle = (target: MaybeRef<HTMLElement | null | undefined>) => {
 
   const { state, style } = reactiveStyle()
 
+  // Sync existing style from supplied element
   watch(
     targetRef,
     (newVal: HTMLElement | null | undefined) => {
       if (!newVal || !newVal.style) return
 
       for (const key of Object.keys(valueTypes)) {
-        // @ts-expect-error
-        if (newVal.style[key] === undefined) continue
-        // @ts-expect-error
-        if (!newVal.style[key]) state[key] = newVal.style[key]
+        if (
+          newVal.style[key] === undefined ||
+          newVal.style[key] === null ||
+          newVal.style[key] === ''
+        )
+          continue
+
+        state[key] = newVal.style[key]
       }
     },
     {
@@ -25,11 +30,18 @@ export const useStyle = (target: MaybeRef<HTMLElement | null | undefined>) => {
     },
   )
 
-  watch(style, (newValue) => {
-    if (!targetRef || !targetRef.value || !newValue) return
+  // Sync reactive style to element
+  watch(
+    style,
+    (newValue) => {
+      if (!targetRef || !targetRef.value || !newValue) return
 
-    Object.assign(targetRef.value.style, newValue)
-  })
+      Object.assign(targetRef.value.style, newValue)
+    },
+    {
+      immediate: true,
+    },
+  )
 
   return {
     style: state,
