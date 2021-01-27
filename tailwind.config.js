9372346dc47417eafed5b539608a0513250c7b06
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-27 15:29:50 +0100

    :wrench: (tailwind) add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./demo/index.html', './demo/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
