ce8b6e2e237bbff52df9d66618a5487e309f1a87
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-27 19:25:21 +0100

    :sparkles: (useMotion) use  ref instead of AMD_ENTRYPOINT=vs/workbench/services/extensions/node/extensionHostProcess APPLICATION_INSIGHTS_NO_DIAGNOSTIC_CHANNEL=true BASH=/bin/sh BASH_ARGC=() BASH_ARGV=() BASH_EXECUTION_STRING='git commit -m ":sparkles: (useMotion) use `name` ref instead of `set` callback to handle current variant"' BASH_LINENO=() BASH_SOURCE=() BASH_VERSINFO=([0]="3" [1]="2" [2]="57" [3]="1" [4]="release" [5]="x86_64-apple-darwin20") BASH_VERSION='3.2.57(1)-release' CI=true COMMAND_MODE=unix2003 DIRSTACK=() ELECTRON_RUN_AS_NODE=1 EUID=501 GOOGLE_APPLICATION_CREDENTIALS=/Users/tahul/.google-key.json GROUPS=() HOME=/Users/tahul HOSTNAME=macbook-pro-de-yael.home HOSTTYPE=x86_64 IFS=' ' LDFLAGS=-L/usr/local/opt/libffi/lib LESS=-R LOGNAME=tahul LSCOLORS=Gxfxcxdxbxegedabagacad LaunchInstanceID=1CC41F71-A95B-44F8-89CF-497D5A40C940 MACHTYPE=x86_64-apple-darwin20 OPTERR=1 OPTIND=1 ORIGINAL_XDG_CURRENT_DESKTOP=undefined OSTYPE=darwin20 PAGER=less PATH='/Users/tahul/.volta/bin:/Users/tahul/google-cloud-sdk/bin:/Users/tahul/.composer/vendor/bin:/Users/tahul/Library/Python/3.7/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin' PIPE_LOGGING=true PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig POSIXLY_CORRECT=y PPID=85111 PS4='+ ' PWD=/Users/tahul/Projets/sandbox/vue-use-motion SECURITYSESSIONID=186b6 SHELL=/bin/zsh SHELLOPTS=braceexpand:hashall:interactive-comments:posix SHLVL=1 SPACESHIP_ROOT=/Users/tahul/.oh-my-zsh/custom/themes/spaceship-prompt SPACESHIP_VERSION=3.11.2 SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.hzFuHLdZRc/Listeners TERM=dumb TMPDIR=/var/folders/5w/mb6mcv1537917p38yq4ytnnw0000gn/T/ UID=501 USER=tahul VERBOSE_LOGGING=true VOLTA_HOME=/Users/tahul/.volta VSCODE_HANDLES_UNCAUGHT_ERRORS=true VSCODE_IPC_HOOK='/Users/tahul/Library/Application Support/Code/1.52.1-main.sock' VSCODE_IPC_HOOK_EXTHOST=/var/folders/5w/mb6mcv1537917p38yq4ytnnw0000gn/T/vscode-ipc-23da729f-b04c-48d6-9443-80a0d875f7e5.sock VSCODE_LOG_STACK=false VSCODE_NLS_CONFIG='{"locale":"en-us","availableLanguages":{},"_languagePackSupport":true}' VSCODE_NODE_CACHED_DATA_DIR='/Users/tahul/Library/Application Support/Code/CachedData/ea3859d4ba2f3e577a159bc91e3074c5d85c0523' VSCODE_PID=928 XPC_FLAGS=0x0 XPC_SERVICE_NAME=application.com.microsoft.VSCode.51674410.51674416 ZSH=/Users/tahul/.oh-my-zsh _='/Applications/Visual Studio Code.app/Contents/MacOS/Electron' __CFBundleIdentifier=com.microsoft.VSCode __CF_USER_TEXT_ENCODING=0x1F5:0x0:0x1 callback to handle current variant

--- src/useMotion.ts
+++ src/useMotion.ts
@@ -1,18 +1,20 @@
 import { MaybeRef } from '@vueuse/shared'
-import { ref } from 'vue'
+import { Ref, ref } from 'vue'
 import { MotionVariants } from './types/variants'
 import { useMotionControls } from './useMotionControls'
 import { useMotionProperties } from './useMotionProperties'
 import { useMotionVariants } from './useMotionVariants'
 
-export const useMotion = (
+export function useMotion<T extends MotionVariants>(
   target: MaybeRef<HTMLElement | null | undefined>,
-  variants: MotionVariants = {},
-) => {
+  variants: MaybeRef<T> = {} as MaybeRef<T>,
+) {
+  // Base references
+  const variantsRef = ref(variants) as Ref<T>
   const targetRef = ref(target)
 
   // Variants manager
-  const { set, currentVariant } = useMotionVariants(variants)
+  const { name, state: currentVariant } = useMotionVariants(variantsRef)
 
   // Reactive styling and transform
   const { style, transform } = useMotionProperties(targetRef)
@@ -21,7 +23,7 @@ export const useMotion = (
   const { stop } = useMotionControls(transform, style, currentVariant)
 
   return {
-    set,
+    name,
     stop,
   }
 }
