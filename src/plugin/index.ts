38c40e75cccee3fe989087f1a64e4d4bc7b2c5d8
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-28 21:35:31 +0100

    :sparkles: (plugin) implement plugin

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,12 +1,31 @@
+import { MotionVariants, Variant } from '@lib/types/variants'
+import { useMotion } from '@lib/useMotion'
 import { Plugin } from 'vue'
 import { directive } from '../directive'
+
+export type MotionDirectiveInjection = {
+  [key: string]: ReturnType<typeof useMotion>
+}
+
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    $motions?: MotionDirectiveInjection
+  }
+}
+
+declare module '@vue/runtime-dom' {
+  interface HTMLAttributes {
+    variants?: MotionVariants
+    initial?: Variant
+    enter?: Variant
+    leave?: Variant
+  }
+}
 
 export function plugin(): Plugin {
   return {
     install(app, options) {
-      app.directive('motion', directive)
+      app.directive('motion', directive())
     },
   }
 }
-
-export default plugin()
