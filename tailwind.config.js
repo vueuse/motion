8d1a1704e73b0cd780ba73fe9afc2bdea4685fc6
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-28 21:34:58 +0100

    :wrench: (tw) import tailwind colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./demo/index.html', './demo/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      ...colors,
+    },
     extend: {},
   },
   variants: {
