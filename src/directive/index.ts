17b09967f0ecc91fb08a1e45d4873831686017ea
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-28 21:52:24 +0100

    :sparkles: (directive) implement v-motion directive

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,15 +1,87 @@
-import { Directive } from 'vue'
+import { Directive, ref } from 'vue'
+import { useMotion } from '../'
+import { MotionVariants } from '../types/variants'
+
+const directivePropsKeys = ['initial', 'enter', 'leave']
+
+const getMotionRef = (binding: any, node: any) => {
+  if (!node || !node.props || !node.props.ref) {
+    throw new Error(
+      'You need to specify a ref on the element when using v-motion.',
+    )
+  }
+
+  return binding.instance.$motions[node.props.ref.toString()]
+}
+
+const getVariantsRef = (node: any) => {
+  const variantsRef = ref<MotionVariants>({})
+
+  variantsRef.value = directivePropsKeys.reduce<MotionVariants>(
+    (prev, curr) => {
+      if (node && node.props && node.props[curr]) {
+        prev[curr] = node.props[curr]
+      }
+
+      return prev
+    },
+    {},
+  )
+
+  return variantsRef
+}
 
 export function directive(): Directive {
   return {
-    created() {},
-    beforeMount() {},
-    mounted() {},
-    beforeUpdate() {},
-    updated() {},
-    beforeUnmount() {},
+    created(el, binding, node) {
+      if (!node || !node.props || !node.props.ref) {
+        throw new Error(
+          'You need to specify a ref on the element when using v-motion.',
+        )
+      }
+
+      const targetRef = ref<HTMLElement>(el)
+
+      const motionRef = useMotion(targetRef, getVariantsRef(node), {
+        lifeCycleHooks: false,
+      })
+
+      if (binding && binding.instance) {
+        if (!binding.instance.$motions) {
+          binding.instance.$motions = {}
+        }
+
+        binding.instance.$motions[node.props.ref.toString()] = motionRef
+      }
+    },
+    beforeMount(el, binding, node) {
+      const variants = getVariantsRef(node)
+      const motion = getMotionRef(binding, node)
+
+      console.log({ variants, motion })
+
+      if (variants.value && variants.value['initial']) {
+        motion.variant.value = 'initial'
+      }
+    },
+    mounted(el, binding, node) {
+      const variants = getVariantsRef(node)
+      const motion = getMotionRef(binding, node)
+
+      if (variants.value && variants.value['enter']) {
+        motion.variant.value = 'enter'
+      }
+    },
+    beforeUnmount(el, binding, node) {
+      const variants = getVariantsRef(node)
+      const motion = getMotionRef(binding, node)
+
+      if (variants.value && variants.value['leave']) {
+        motion.variant.value = 'leave'
+      }
+    },
     unmounted() {},
   }
 }
 
-export default directive()
+export default directive
