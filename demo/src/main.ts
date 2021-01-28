d4c9cac4e4949ab7fe6f8dce8090481ed4ba7b4c
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-28 21:51:14 +0100

    :sparkles: (plugin) setup plugin in demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,7 +1,10 @@
+import { plugin } from '@lib'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
 const app = createApp(App)
+
+app.use(plugin())
 
 app.mount('#app')
