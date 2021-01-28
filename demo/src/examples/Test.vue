2cb619597fd91b7f04aeb98483153572051d5150
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-28 21:51:45 +0100

    :sparkles: (demo) add test component

---
+++ demo/src/examples/Test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div
+      v-motion
+      ref="title"
+      :initial="{ opacity: 0, y: 100 }"
+      :enter="{
+        opacity: 1,
+        y: 0,
+      }"
+    >
+      Hello
+    </div>
+
+    <div
+      v-motion
+      ref="title2"
+      :initial="{ opacity: 0, y: 100 }"
+      :enter="{
+        opacity: 1,
+        y: 0,
+      }"
+    >
+      Hello
+    </div>
+
+    <div
+      v-motion
+      ref="title3"
+      :initial="{ opacity: 0, y: 100 }"
+      :enter="{
+        opacity: 1,
+        y: 0,
+      }"
+    >
+      Hello
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useMotions } from '@lib'
+import { nextTick, onMounted } from 'vue'
+
+const motions = useMotions()
+
+onMounted(() => {
+  console.log(motions)
+})
+</script>
