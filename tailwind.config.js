15d1c10c7fdea7b3018b1286a1c48f3f09f6b64f
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-29 23:50:01 +0100

    :green_heart: (ci) fix ci purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ['./demo/index.html', './demo/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
