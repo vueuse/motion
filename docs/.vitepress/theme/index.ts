925d8663bbbf440ae23995cdc806946678e78b0d
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-29 20:58:19 +0100

    :sparkles: (docs) use MotionPlugin from docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,11 @@
 import DefaultTheme from 'vitepress/dist/client/theme-default'
-import { plugin } from '../../../src'
+import { MotionPlugin } from '../../../src'
 import Features from '../components/Features.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.use(plugin())
+    app.use(MotionPlugin)
 
     app.component('Features', Features)
   },
