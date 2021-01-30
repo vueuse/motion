5eac27b55e28391138e600c9737ff5d8b5e3f3b2
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-30 19:28:19 +0100

    :sparkles: (reactiveStyle) comment the file

--- src/reactiveStyle.ts
+++ src/reactiveStyle.ts
@@ -2,17 +2,28 @@ import { computed, reactive } from 'vue'
 import { StyleProperties } from './types/variants'
 import { getValueAsType, getValueType } from './utils/style'
 
+/**
+ * Reactive style object implementing all native CSS properties.
+ *
+ * @param props
+ */
 export function reactiveStyle(props: StyleProperties = {}) {
+  // Reactive StyleProperties object
   const state = reactive<StyleProperties>({
     ...props,
   })
 
+  // Reactive DOM Element compatible `style` object bound to state
   const style = computed<StyleProperties>(() => {
+    // Init result object
     const result: StyleProperties = {}
 
     for (const [key, value] of Object.entries(state)) {
+      // Get value type for key
       const valueType = getValueType(key)
+      // Get value as type for key
       const valueAsType = getValueAsType(value, valueType)
+      // Append the computed style to result object
       result[key] = valueAsType
     }
 
