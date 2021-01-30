80f01c48828a511e5b8e730da96dbf132be8ae9a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-30 19:32:47 +0100

    :sparkles: (useMotionProperties) comment the file

--- src/useMotionProperties.ts
+++ src/useMotionProperties.ts
@@ -3,13 +3,21 @@ import { ref } from 'vue'
 import { useStyle } from './useStyle'
 import { useTransform } from './useTransform'
 
+/**
+ * A Composable giving access to both `transform` and `style`objects for a single element.
+ *
+ * @param target
+ */
 export function useMotionProperties(
   target: MaybeRef<HTMLElement | null | undefined>,
 ) {
+  // Target element ref
   const targetRef = ref(target)
 
+  // Target element style object
   const { style } = useStyle(targetRef)
 
+  // Target element transform object
   const { transform } = useTransform(targetRef)
 
   return {
