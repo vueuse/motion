65380e142c420e2f2809afb8bfcb3865a554bdb2
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-30 19:37:06 +0100

    :sparkles: (editor) update editor animations

--- demo/src/components/Editor.vue
+++ demo/src/components/Editor.vue
@@ -1,40 +1,57 @@
 <template>
   <div
-    v-motion
-    ref="block"
+    class="w-full p-6 bg-gray-800 border-4 shadow-2xl rounded-3xl border-violet-600"
+    v-motion="'block'"
     :initial="{
-      y: 200,
-      opacity: 0.25,
+      y: 100,
+      scale: 0.9,
+      opacity: 0,
     }"
     :enter="{
       y: 0,
+      scale: 1,
       opacity: 1,
+      transition: {
+        delay: 400,
+      },
     }"
   >
-    <div class="w-full p-6 bg-gray-800 rounded-3xl">
-      <div class="flex items-center justify-center w-full h-96">
-        <div class="flex-1">
-          <code-block :code-text="basic" />
-        </div>
-        <div class="flex items-center justify-center flex-1">
-          <div
-            v-motion
-            ref="demo"
-            :initial="{
-              y: 200,
-              opacity: 0.25,
-            }"
-            :enter="{
-              y: 0,
-              opacity: 1,
-              transition: {
-                type: 'spring',
-                delay: 100,
-              },
-            }"
-            class="w-24 h-24 bg-indigo-500 shadow-lg rounded-3xl"
-          />
-        </div>
+    <div class="flex items-center justify-center w-full h-96">
+      <div
+        v-motion="'code'"
+        :initial="{
+          y: 100,
+          opacity: 0,
+        }"
+        :enter="{
+          y: 0,
+          opacity: 1,
+          transition: {
+            type: 'spring',
+            delay: 500,
+          },
+        }"
+        class="flex-1"
+      >
+        <code-block :code-text="basic" language="javascript" />
+      </div>
+      <div class="flex items-center justify-center flex-1">
+        <div
+          v-motion="'demo'"
+          :initial="{
+            y: 100,
+            opacity: 0,
+          }"
+          :enter="{
+            y: 0,
+            opacity: 1,
+            transition: {
+              type: 'spring',
+              delay: 600,
+            },
+          }"
+          class="w-24 h-24 bg-indigo-500 shadow-lg rounded-3xl"
+        />
       </div>
     </div>
   </div>
@@ -44,5 +61,3 @@
 import CodeBlock from '../components/CodeBlock.vue'
 import basic from '../examples/basic'
 </script>
-
-<style lang="scss" scoped></style>
