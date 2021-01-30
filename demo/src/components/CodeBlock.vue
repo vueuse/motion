4046f31034f2f9f5d4e13af2e0418c4ed3ed148d
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-30 12:58:30 +0100

    :recycle: (codeblock) update codeblock to <script setup> syntax

--- demo/src/components/CodeBlock.vue
+++ demo/src/components/CodeBlock.vue
@@ -1,58 +1,53 @@
 <template>
   <pre ref="pre" class="w-full shadow-xl">
     <code ref="code" :class="['w-full', `language-${language}`]">
-        <slot />
+        <slot v-if="$slots" />
     </code>
 </pre>
 </template>
 
-<script lang="ts">
+<script setup="props" lang="ts">
+import { onMounted, ref, nextTick, defineProps } from 'vue'
 import Prism from 'prismjs'
-import { onMounted, ref, nextTick } from 'vue'
 
-export default {
-  props: {
-    codeText: {
-      type: String,
-      default: '',
-    },
-    language: {
-      type: String,
-      default: 'javascript',
-    },
+const { language, codeText } = defineProps({
+  language: {
+    type: String,
+    default: 'javascript',
   },
-  setup({ language, codeText }) {
-    // Refs
-    const pre = ref<string>()
-    const code = ref<HTMLElement>()
-
-    // Functions
-    const preRender = (codeContent: string) => {
-      return codeContent.replace(/\s+data-v-\S+="[^"]*"/g, '')
-    }
-    const render = () => {
-      if (!Prism.languages[language]) {
-        require(`prismjs/components/prism-${language}`)
-      }
-
-      nextTick(() => {
-        const codeContent = codeText || code.value.innerText
-
-        code.value.textContent = preRender(codeContent)
-        Prism.highlightElement(code.value)
-      })
-    }
-
-    // Hooks
-    onMounted(() => {
-      render()
-    })
-
-    return {
-      pre,
-      code,
-      language,
-    }
+  codeText: {
+    type: String,
+    default: '',
   },
+})
+
+// Refs
+ref: pre = ref<string>()
+ref: code = ref<HTMLElement>()
+
+// Functions
+const preRender = (codeContent: string) => {
+  return codeContent.replace(/\s+data-v-\S+="[^"]*"/g, '')
 }
+
+const render = () => {
+  if (!Prism.languages[language]) {
+    require(`prismjs/components/prism-${language}`)
+  }
+
+  nextTick(() => {
+    if (!code) return
+
+    const codeContent = codeText || code.innerText
+
+    code.textContent = preRender(codeContent)
+
+    Prism.highlightElement(code)
+  })
+}
+
+// Hooks
+onMounted(() => {
+  render()
+})
 </script>
