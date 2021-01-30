df37b8bf3c1e1131e43349497c2d89073f4d35e7
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-30 19:30:06 +0100

    :sparkles: (useMotion) comment the file, implement useMotionFeatures

--- src/useMotion.ts
+++ src/useMotion.ts
@@ -2,18 +2,30 @@ import { MaybeRef } from '@vueuse/shared'
 import { Ref, ref } from 'vue'
 import { MotionVariants } from './types/variants'
 import { useMotionControls } from './useMotionControls'
+import { useMotionFeatures } from './useMotionFeatures'
 import { useMotionProperties } from './useMotionProperties'
 import { useMotionVariants } from './useMotionVariants'
 
 export type UseMotionOptions = {
   lifeCycleHooks?: boolean
+  visibilityHooks?: boolean
 }
 
+/**
+ * A Vue Composable that put your components in motion.
+ *
+ * @docs https://vue-use-sound.netlify.app
+ *
+ * @param target
+ * @param variants
+ * @param options
+ */
 export function useMotion<T extends MotionVariants>(
   target: MaybeRef<HTMLElement | null | undefined>,
   variants: MaybeRef<T> = {} as MaybeRef<T>,
   options: UseMotionOptions = {
     lifeCycleHooks: true,
+    visibilityHooks: true,
   },
 ) {
   // Base references
@@ -24,11 +36,13 @@ export function useMotion<T extends MotionVariants>(
   const { variant, state: currentVariant } = useMotionVariants<T>(
     variantsRef,
     'initial',
-    options,
   )
 
   // Reactive styling and transform
   const { style, transform } = useMotionProperties(targetRef)
+
+  // Bind features
+  useMotionFeatures(targetRef, variant, variants, options)
 
   // Motion controls, synchronized with styling and variants
   const { stop } = useMotionControls(transform, style, currentVariant)
