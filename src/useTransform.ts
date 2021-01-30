b2566538317248d24bdc6761aebfa11eaf76c599
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-30 19:31:37 +0100

    :sparkles: (useTransform) comment the file

--- src/useTransform.ts
+++ src/useTransform.ts
@@ -2,28 +2,20 @@ import { MaybeRef } from '@vueuse/shared'
 import { ref, watch } from 'vue-demi'
 import { reactiveTransform } from './reactiveTransform'
 
+/**
+ * A Composable giving access to a TransformProperties object, and binding the generated transform string to a target.
+ *
+ * @param target
+ */
 export function useTransform(target: MaybeRef<HTMLElement | null | undefined>) {
+  // Target element ref
   const targetRef = ref(target)
 
+  // Transform cache available before the element is mounted
   const _cache = ref<string>()
 
+  // Create a reactive transform object
   const { state, transform } = reactiveTransform()
-
-  // Sync reactive transform to element
-  watch(
-    transform,
-    (newValue) => {
-      if (!targetRef || !targetRef.value) {
-        _cache.value = newValue
-        return
-      }
-
-      targetRef.value.style.transform = newValue
-    },
-    {
-      immediate: true,
-    },
-  )
 
   // Cache transform until the element is alive and we can bind to it
   watch(
@@ -32,8 +24,27 @@ export function useTransform(target: MaybeRef<HTMLElement | null | undefined>) {
       if (!newValue) return
 
       if (_cache && _cache.value) {
+        // If cache is present, init the target with the current cached value
         newValue.style.transform = _cache.value
       }
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  // Sync reactive transform to element
+  watch(
+    transform,
+    (newValue) => {
+      if (!targetRef || !targetRef.value) {
+        // Add the current value to the cache so it is set on target creation
+        _cache.value = newValue
+        return
+      }
+
+      // Set the transform string on the target
+      targetRef.value.style.transform = newValue
     },
     {
       immediate: true,
