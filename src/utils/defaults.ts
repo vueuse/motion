bfd35ea6736e67390f0cad5e18737d2ca7acdcc3
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-31 23:50:59 +0100

    :sparkles: (defaults) import default transitions from framer-motion

---
+++ src/utils/defaults.ts
@@ -0,0 +1,74 @@
+import {
+  Keyframes,
+  KeyframesTarget,
+  PopmotionTransitionProps,
+  SingleTarget,
+  ValueTarget,
+} from '../types/transitions'
+
+export const isKeyframesTarget = (v: ValueTarget): v is KeyframesTarget => {
+  return Array.isArray(v)
+}
+
+export const underDampedSpring = () => ({
+  type: 'spring',
+  stiffness: 500,
+  damping: 25,
+  restDelta: 0.5,
+  restSpeed: 10,
+})
+
+export const overDampedSpring = (to: SingleTarget) => ({
+  type: 'spring',
+  stiffness: 550,
+  damping: to === 0 ? 100 : 30,
+  restDelta: 0.01,
+  restSpeed: 10,
+})
+
+export const linearTween = () => ({
+  type: 'keyframes',
+  ease: 'linear',
+  duration: 0.3,
+})
+
+const keyframes = (values: KeyframesTarget): Partial<Keyframes> => ({
+  type: 'keyframes',
+  duration: 0.8,
+  values,
+})
+
+type TransitionFactory = (to: ValueTarget) => Partial<PopmotionTransitionProps>
+
+const defaultTransitions = {
+  x: underDampedSpring,
+  y: underDampedSpring,
+  z: underDampedSpring,
+  rotate: underDampedSpring,
+  rotateX: underDampedSpring,
+  rotateY: underDampedSpring,
+  rotateZ: underDampedSpring,
+  scaleX: overDampedSpring,
+  scaleY: overDampedSpring,
+  scale: overDampedSpring,
+  opacity: linearTween,
+  backgroundColor: linearTween,
+  color: linearTween,
+  default: overDampedSpring,
+}
+
+export const getDefaultTransition = (
+  valueKey: string,
+  to: ValueTarget,
+): PopmotionTransitionProps => {
+  let transitionFactory: TransitionFactory
+
+  if (isKeyframesTarget(to)) {
+    transitionFactory = keyframes as TransitionFactory
+  } else {
+    transitionFactory =
+      defaultTransitions[valueKey] || defaultTransitions.default
+  }
+
+  return { to, ...transitionFactory(to) } as PopmotionTransitionProps
+}
