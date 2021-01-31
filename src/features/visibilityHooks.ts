8d67cad13da77e553b3b42021655616e6d20674b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-31 23:31:59 +0100

    :sparkles: (visibilityHooks) move intersection observer hooks

---
+++ src/features/visibilityHooks.ts
@@ -0,0 +1,49 @@
+import { MotionVariants } from '@lib/types/variants'
+import { MaybeRef, tryOnUnmounted, useIntersectionObserver } from '@vueuse/core'
+import { ref, watch } from 'vue'
+
+export function registerVisibilityHooks<T extends MotionVariants>(
+  target: MaybeRef<HTMLElement | null | undefined>,
+  variants: MaybeRef<T> = {} as MaybeRef<T>,
+  set: (name: keyof T) => void,
+  revert: () => void,
+) {
+  // Local target ref
+  const targetRef = ref(target)
+  // Variants as ref
+  const variantsRef = ref(variants)
+  // Current observer stop function
+  const _stop = ref<() => void>()
+
+  const stopVisibilityWatch = watch(
+    [variantsRef, targetRef],
+    () => {
+      if (!variantsRef.value || !variantsRef.value.visible || !targetRef) {
+        if (_stop.value) _stop.value()
+        _stop.value = undefined
+        return
+      }
+
+      // Bind intersection observer on target
+      const { stop } = useIntersectionObserver(
+        targetRef,
+        ([{ isIntersecting }]) => {
+          if (isIntersecting) {
+            set('visible')
+          } else revert()
+        },
+      )
+
+      _stop.value = stop
+    },
+    { immediate: true, flush: 'pre' },
+  )
+
+  tryOnUnmounted(() => {
+    if (_stop && _stop.value) {
+      _stop.value()
+    }
+
+    stopVisibilityWatch()
+  })
+}
