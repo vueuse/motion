81619da5ba0d0a6c178256e62cca16a5eff6eb46
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-01 23:32:27 +0100

    :sparkles: (demo) editor trial w/ rotate example

--- demo/src/components/Editor.vue
+++ demo/src/components/Editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative w-full overflow-hidden bg-gray-800 border-4 shadow-2xl rounded-xl border-violet-600"
+    class="relative w-full p-4 overflow-hidden bg-gray-800 border-4 shadow-2xl rounded-xl border-violet-600"
     v-motion
     :initial="{
       y: 200,
@@ -15,38 +15,42 @@
   >
     <div class="flex items-center w-full h-full">
       <div class="flex-1">
-        <code-block :code-text="basic" language="javascript" />
+        <code-block :code-text="codeText" language="javascript" />
       </div>
       <div class="flex items-center justify-center flex-1">
-        <div v-if="visible">
-          <div
-            v-motion="'test'"
-            :initial="{
-              x: 0,
-              y: 100,
-              opacity: 0,
-            }"
-            :enter="{
-              y: 0,
-              x: 0,
-              opacity: 1,
-              transition: {
-                delay: 400,
-                type: 'spring',
-                bounce: 0.3,
-              },
-            }"
-            class="w-24 h-24 bg-indigo-500 border-8 border-indigo-400 shadow-xl rounded-3xl"
-          />
-        </div>
+        <div
+          v-if="visible"
+          v-motion="'test'"
+          :initial="{
+            x: 0,
+            y: 100,
+            opacity: 0,
+            rotate: 0,
+          }"
+          :enter="{
+            y: 0,
+            x: 0,
+            rotate: 0,
+            opacity: 1,
+            transition: {
+              delay: 400,
+              type: 'spring',
+            },
+          }"
+          class="w-24 h-24 bg-indigo-500 border-8 border-indigo-400 shadow-xl rounded-3xl"
+        />
       </div>
     </div>
 
     <div
       @click="replay"
-      class="absolute transform -rotate-90 cursor-pointer select-none top-4 right-8"
+      class="absolute transform -rotate-90 cursor-pointer select-none top-6 right-8"
     >
       <span> 🔃 </span>
+    </div>
+
+    <div>
+      <input type="range" max="180" min="0" v-model="input" step="1" />
     </div>
   </div>
 </template>
@@ -54,7 +58,7 @@
 <script setup="props" lang="ts">
 import { useMotions } from '@lib/useMotions'
 import { useToggle } from '@vueuse/core'
-import { nextTick, ref, watch } from 'vue'
+import { computed, nextTick, ref, watch } from 'vue'
 import CodeBlock from '../components/CodeBlock.vue'
 import basic from '../examples/basic'
 
@@ -63,17 +67,23 @@ const [visible, toggleVisible] = useToggle(true)
 const replay = () => {
   toggleVisible()
 
+  input.value = '0'
+
   nextTick(() => toggleVisible())
 }
 
 const motions = useMotions()
 
-const input = ref<number>(0)
+const input = ref<string>('0')
+
+const codeText = computed(() => {
+  return basic(input.value)
+})
 
 watch(input, (newVal) => {
   if (motions.test) {
     motions.test.apply({
-      x: newVal,
+      rotate: newVal,
       transition: {
         type: 'spring',
       },
