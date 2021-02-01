33baa7ac2f99b13f03d2ddf16cccafd0c6899d04
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-01 20:20:13 +0100

    :sparkles: (useMotions) return global state on useMotions

--- src/useMotions.ts
+++ src/useMotions.ts
@@ -1,29 +1,5 @@
-import { getCurrentInstance, onMounted, shallowReactive } from 'vue'
-import { MotionDirectiveInjection } from './plugin'
+import { motionState } from './features/state'
 
-/**
- * A Composable giving access to current component instance `$motions` instances declared via `v-motion` from the template.
- */
 export function useMotions() {
-  // Get the current component instance
-  const app = getCurrentInstance()
-
-  // Create a shallow reactive object, holding motions
-  const motions = shallowReactive<MotionDirectiveInjection>({})
-
-  // On mounted, get all the $motions, and bind them to the shallow reactive object
-  onMounted(() => {
-    // Check if the current component instance has $motions injected
-    if (app?.proxy?.$motions) {
-      // Append the local shallowReactive object with all the $motions injected in the component instance
-      Object.assign(motions, app.proxy.$motions)
-    } else {
-      // If there's no v-motions in the component, this hook shouldn't be present in setup().
-      console.warn(
-        `Could not find any motion in this component, are you using v-motion anywhere?`,
-      )
-    }
-  })
-
-  return motions
+  return motionState
 }
