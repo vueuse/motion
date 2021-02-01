655bdbf545978d4b9d04d3b1c60c813d383951aa
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-01 20:15:38 +0100

    :sparkles: (plugin) lighten plugin file by moving types

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,27 +1,5 @@
-import { MotionVariants, Variant } from '@lib/types/variants'
-import { useMotion } from '@lib/useMotion'
 import { Plugin } from 'vue'
 import { directive } from '../directive'
-
-export type MotionDirectiveInjection = {
-  [key: string]: ReturnType<typeof useMotion>
-}
-
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $motions?: MotionDirectiveInjection
-  }
-}
-
-declare module '@vue/runtime-dom' {
-  interface HTMLAttributes {
-    variants?: MotionVariants
-    initial?: Variant
-    enter?: Variant
-    leave?: Variant
-    visible?: Variant
-  }
-}
 
 export const MotionPlugin: Plugin = {
   install(app) {
