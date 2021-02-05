4ba1e60f6bc9ce9e4f3ede4f3fe03d22c6c4a5af
Anthony Fu <anthonyfu117@hotmail.com>
2021-02-05 14:22:10 +0800

    refactor: registerVisibilityHooks
    - useIntersectionObserver already handles rebinding on el changes
    - useIntersectionObserver calls tryOnUnmounted already

--- src/features/visibilityHooks.ts
+++ src/features/visibilityHooks.ts
@@ -1,5 +1,5 @@
-import { MaybeRef, tryOnUnmounted, useIntersectionObserver } from '@vueuse/core'
-import { ref, watch } from 'vue-demi'
+import { MaybeRef, useIntersectionObserver } from '@vueuse/core'
+import { ref } from 'vue-demi'
 import { TargetType, MotionVariants } from '../types'
 
 export function registerVisibilityHooks<T extends MotionVariants>(
@@ -11,38 +11,22 @@ export function registerVisibilityHooks<T extends MotionVariants>(
   const targetRef = ref(target)
   // Variants as ref
   const variantsRef = ref(variants)
-  // Current observer stop function
-  const _stop = ref<() => void>()
 
-  const stopVisibilityWatch = watch(
-    [variantsRef, targetRef],
-    () => {
-      if (!variantsRef.value || !variantsRef.value.visible || !targetRef) {
-        if (_stop.value) _stop.value()
-        _stop.value = undefined
-        return
+  // Bind intersection observer on target
+  const { stop } = useIntersectionObserver(
+    targetRef,
+    ([{ isIntersecting }]) => {
+      if (variantsRef.value?.visible) {
+        if (isIntersecting) {
+          set('visible')
+        } else {
+          set('initial')
+        }
       }
-
-      // Bind intersection observer on target
-      const { stop } = useIntersectionObserver(
-        targetRef,
-        ([{ isIntersecting }]) => {
-          if (isIntersecting) {
-            set('visible')
-          } else set('initial')
-        },
-      )
-
-      _stop.value = stop
     },
-    { immediate: true, flush: 'pre' },
   )
 
-  tryOnUnmounted(() => {
-    if (_stop && _stop.value) {
-      _stop.value()
-    }
-
-    stopVisibilityWatch()
-  })
+  return {
+    stop,
+  }
 }
