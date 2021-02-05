49f0ae25950a9063363763002a7db579ad004bca
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-05 01:29:54 +0100

    :pencil: (theme) import layout from vitepress default theme & clean it up

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,17 @@
 import DefaultTheme from 'vitepress/dist/client/theme-default'
 import { MotionPlugin } from '../../../src'
 import Features from '../components/Features.vue'
+import Hero from '../components/Hero.vue'
+import Layout from '../Layout.vue'
 
 export default {
   ...DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.use(MotionPlugin)
 
     app.component('Features', Features)
+
+    app.component('Hero', Hero)
   },
 }
