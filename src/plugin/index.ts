210f6269b3e8ccf4085eda38d5060cb3332a3c5b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-06 14:08:51 +0100

    :sparkles: (plugin) support custom directives from plugin options

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,9 +1,25 @@
 import { Plugin } from 'vue-demi'
 import { directive } from '../directive'
+import { MotionVariants } from '../types'
 
 export const MotionPlugin: Plugin = {
-  install(app) {
-    app.directive('motion', directive)
+  install(app, options) {
+    // Register default `v-motion` directive.
+    app.directive('motion', directive())
+
+    // Loop on options, create a custom directive for each definition.
+    for (const key in options) {
+      // Get directive variants
+      const variants = options[key] as MotionVariants
+
+      // Development warning, showing definitions missing `initial` key.
+      if (!variants.initial && __DEV__) {
+        console.warn(`Your variant v-motion-${key} is missing initial variant!`)
+      }
+
+      // Register the custom `v-motion-${key}` directive directive.
+      app.directive(`motion-${key}`, directive(variants))
+    }
   },
 }
 
