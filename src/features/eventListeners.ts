99aec0bf7dfcf82d25ba3ba606feec6536f02781
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-07 19:39:35 +0100

    :sparkles: (eventListeners) only mutate specific event keys when pushing updates

--- src/features/eventListeners.ts
+++ src/features/eventListeners.ts
@@ -18,19 +18,44 @@ export function registerEventListeners<T extends MotionVariants>({
   const tapped = ref(false)
   const focused = ref(false)
 
+  const mutableKeys = computed(() => {
+    let result: string[] = []
+
+    if (variants.value.hovered) {
+      result = [...result, ...Object.keys(variants.value.hovered)]
+    }
+
+    if (variants.value.tapped) {
+      result = [...result, ...Object.keys(variants.value.tapped)]
+    }
+
+    if (variants.value.focused) {
+      result = [...result, ...Object.keys(variants.value.focused)]
+    }
+
+    return result
+  })
+
   const computedProperties = computed(() => {
     const result = {}
 
     Object.assign(result, state.value)
 
-    if (hovered.value && variants.value.hovered)
+    if (hovered.value && variants.value.hovered) {
       Object.assign(result, variants.value.hovered)
+    }
 
-    if (tapped.value && variants.value.tapped)
+    if (tapped.value && variants.value.tapped) {
       Object.assign(result, variants.value.tapped)
+    }
 
-    if (focused.value && variants.value.focused)
+    if (focused.value && variants.value.focused) {
       Object.assign(result, variants.value.focused)
+    }
+
+    for (const key in result) {
+      if (!mutableKeys.value.includes(key)) delete result[key]
+    }
 
     return result
   })
@@ -100,7 +125,7 @@ export function registerEventListeners<T extends MotionVariants>({
   }
 
   // Watch local computed variant, apply it dynamically
-  watch(computedProperties, () => {
-    apply(computedProperties.value)
+  watch(computedProperties, (newVal) => {
+    apply(newVal)
   })
 }
