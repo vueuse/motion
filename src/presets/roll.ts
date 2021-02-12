b77cf57243d99f72b50c9d8b83c82718d80b69fb
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-12 22:31:50 +0100

    :sparkles: (presets) try visible implem for roll

--- src/presets/roll.ts
+++ src/presets/roll.ts
@@ -13,6 +13,19 @@ export const rollLeft: MotionVariants = {
   },
 }
 
+export const rollVisibleLeft: MotionVariants = {
+  initial: {
+    x: 100,
+    rotate: 90,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    rotate: 0,
+    opacity: 1,
+  },
+}
+
 export const rollRight: MotionVariants = {
   initial: {
     x: -100,
@@ -20,6 +33,19 @@ export const rollRight: MotionVariants = {
     opacity: 0,
   },
   enter: {
+    x: 0,
+    rotate: 0,
+    opacity: 1,
+  },
+}
+
+export const rollVisibleRight: MotionVariants = {
+  initial: {
+    x: -100,
+    rotate: -90,
+    opacity: 0,
+  },
+  visible: {
     x: 0,
     rotate: 0,
     opacity: 1,
@@ -39,6 +65,19 @@ export const rollTop: MotionVariants = {
   },
 }
 
+export const rollVisibleTop: MotionVariants = {
+  initial: {
+    y: -100,
+    rotate: -90,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    rotate: 0,
+    opacity: 1,
+  },
+}
+
 export const rollBottom: MotionVariants = {
   initial: {
     y: 100,
@@ -46,6 +85,19 @@ export const rollBottom: MotionVariants = {
     opacity: 0,
   },
   enter: {
+    y: 0,
+    rotate: 0,
+    opacity: 1,
+  },
+}
+
+export const rollVisibleBottom: MotionVariants = {
+  initial: {
+    y: 100,
+    rotate: 90,
+    opacity: 0,
+  },
+  visible: {
     y: 0,
     rotate: 0,
     opacity: 1,
