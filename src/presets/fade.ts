a2b3fe819350291e63582fea0182c9e7b03f0470
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-12 22:30:28 +0100

    :sparkles: (presets) add fade preset

---
+++ src/presets/fade.ts
@@ -0,0 +1,14 @@
+import { MotionVariants } from '../types'
+
+export const pop: MotionVariants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+  },
+}
+
+export default {
+  pop,
+}
