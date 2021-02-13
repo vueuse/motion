6d95c6f9f0bc3ed063a8c04b51c899b92e13e81c
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-13 18:30:40 +0100

    :sparkles: (plugin) move plugin-level directives to options.directives

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -2,23 +2,38 @@ import { Plugin } from 'vue-demi'
 import { directive } from '../directive'
 import { MotionVariants } from '../types'
 
+export interface MotionPluginOptions {
+  directives: {
+    [key: string]: MotionVariants
+  }
+  excludePresets: boolean
+}
+
 export const MotionPlugin: Plugin = {
-  install(app, options) {
+  install(app, options: MotionPluginOptions) {
     // Register default `v-motion` directive.
     app.directive('motion', directive())
 
-    // Loop on options, create a custom directive for each definition.
-    for (const key in options) {
-      // Get directive variants
-      const variants = options[key] as MotionVariants
+    if (options.directives) {
+      // Loop on options, create a custom directive for each definition.
+      for (const key in options.directives) {
+        // Get directive variants
+        const variants = options.directives[key] as MotionVariants
 
-      // Development warning, showing definitions missing `initial` key.
-      if (!variants.initial && __DEV__) {
-        console.warn(`Your variant v-motion-${key} is missing initial variant!`)
+        // Development warning, showing definitions missing `initial` key.
+        if (!variants.initial && __DEV__) {
+          console.warn(
+            `Your variant v-motion-${key} is missing initial variant!`,
+          )
+        }
+
+        // Register the custom `v-motion-${key}` directive directive.
+        app.directive(`motion-${key}`, directive(variants))
       }
+    }
 
-      // Register the custom `v-motion-${key}` directive directive.
-      app.directive(`motion-${key}`, directive(variants))
+    if (!options.excludePresets) {
+      // TODO: Implement presets
     }
   },
 }
