b2d78e107fcbe8cc09be5bd689ba3f855dea87e7
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-14 13:59:38 +0100

    :sparkles: (utils) add slugify, for presets

---
+++ src/utils/slugify.ts
@@ -0,0 +1,31 @@
+/**
+ * Convert a string to a slug.
+ *
+ * Source: https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
+ * Credits: @hagemann
+ *
+ * Edited to transform camel naming to slug with `-`.
+ *
+ * @param str
+ */
+export function slugify(string: string) {
+  const a =
+    'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b =
+    'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string
+    .toString()
+    .replace(/[A-Z]/g, (s) => '-' + s) // Camel to slug
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+
+export default slugify
