e109b9c6e47f543be13996533de3a45bc3e55012
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-14 14:08:56 +0100

    :sparkles: (plugin) register each preset

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,6 +1,8 @@
 import { Plugin } from 'vue-demi'
 import { directive } from '../directive'
+import presets from '../presets'
 import { MotionVariants } from '../types'
+import slugify from '../utils/slugify'
 
 export interface MotionPluginOptions {
   directives: {
@@ -11,29 +13,37 @@ export interface MotionPluginOptions {
 
 export const MotionPlugin: Plugin = {
   install(app, options: MotionPluginOptions) {
-    // Register default `v-motion` directive.
+    // Register default `v-motion` directive
     app.directive('motion', directive())
 
-    if (options.directives) {
-      // Loop on options, create a custom directive for each definition.
+    // Register plugin-wise directives
+    if (options && options.directives) {
+      // Loop on options, create a custom directive for each definition
       for (const key in options.directives) {
         // Get directive variants
         const variants = options.directives[key] as MotionVariants
 
-        // Development warning, showing definitions missing `initial` key.
+        // Development warning, showing definitions missing `initial` key
         if (!variants.initial && __DEV__) {
           console.warn(
             `Your variant v-motion-${key} is missing initial variant!`,
           )
         }
 
-        // Register the custom `v-motion-${key}` directive directive.
+        // Register the custom `v-motion-${key}` directive
         app.directive(`motion-${key}`, directive(variants))
       }
     }
 
-    if (!options.excludePresets) {
-      // TODO: Implement presets
+    // Register presets
+    if (!options || (options && !options.excludePresets)) {
+      for (const key in presets) {
+        // Get preset variants
+        const preset = presets[key]
+
+        // Regisrer the preset `v-motion-${key}` directive
+        app.directive(`motion-${slugify(key)}`, directive(preset))
+      }
     }
   },
 }
