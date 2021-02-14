4a765232d952543c424da64268f7bfc2ada48499
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-14 14:09:36 +0100

    :sparkles: (presets) export all presets from presets index

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,1 +1,11 @@
-export * from './'
+import * as fade from './fade'
+import * as pop from './pop'
+import * as roll from './roll'
+import * as slide from './slide'
+
+export default {
+  ...fade,
+  ...pop,
+  ...roll,
+  ...slide,
+}
