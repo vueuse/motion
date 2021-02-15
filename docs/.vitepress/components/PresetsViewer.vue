08354913bfa73e4dd468eef8631cf45b33cb30b4
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-15 14:03:02 +0100

    :pencil: (presets) prepare presets viewer

---
+++ docs/.vitepress/components/PresetsViewer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="presetsViewer">
+    <div>
+      <h2 v-motion-slide-right>Code 👨‍💻</h2>
+    </div>
+    <div>
+      <h2 v-motion-slide-left>Result 🤹</h2>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+.presetsViewer {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.presetsViewer > div {
+  flex: 1;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+}
+</style>
