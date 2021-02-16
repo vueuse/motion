1805ab838a2145895656b8c82c39eaed7f653f64
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-16 11:24:33 +0100

    :pencil: (presets) improve preset section layout

--- docs/.vitepress/components/PresetSection.vue
+++ docs/.vitepress/components/PresetSection.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="presetSection">
-    <h2>
-      {{ `v-motion-${slugify(name)}` }}
+    <h2 style="text-transform: capitalize">
+      {{ name.replace(/[A-Z]/g, (s) => ' ' + s) }}
 
-      <span @click="replay">Replay</span>
+      <button @click="replay">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          focusable="false"
+          role="img"
+          width="1.2em"
+          height="1.5em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 344 432"
+        >
+          <path
+            d="M171 91q70 0 120 50t50 120.5T291 382t-120.5 50T50 382T0 261h43q0 53 37.5 90.5T171 389t90.5-37.5T299 261t-37.5-90.5T171 133v86L64 112L171 5v86z"
+            style="fill: var(--c-text)"
+          ></path>
+        </svg>
+      </button>
     </h2>
     <div class="content">
       <div class="demoCode">
@@ -15,7 +32,20 @@
       </div>
 
       <div class="demoContainer">
-        <div ref="demoElement" class="demoElement" />
+        <div ref="demoElement" class="demoElement">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
+            <path
+              class="a"
+              fill="#35495e"
+              d="M735.07,67.05V531.58c0,129.83-105.24,235.07-235.07,235.07S264.93,661.41,264.93,531.58V67.05h166.3V531.58a68.77,68.77,0,1,0,137.54,0V67.05Z"
+            />
+            <path
+              class="b"
+              fill="#41b883"
+              d="M901.36,67.05V531.58C901.36,753.25,721.67,933,500,933S98.64,753.25,98.64,531.58V67.05H264.93V531.58c0,129.83,105.25,235.07,235.07,235.07S735.07,661.41,735.07,531.58V67.05Z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   </div>
@@ -67,10 +97,10 @@ h2 {
   justify-content: space-between;
 }
 
-h2 > span {
-  font-size: 16px;
+h2 > button {
+  background-color: transparent;
+  border: none;
   cursor: pointer;
-  user-select: none;
 }
 
 .content {
@@ -88,14 +118,13 @@ h2 > span {
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 8rem;
+  min-height: 12rem;
+  background-color: var(--code-inline-bg-color);
+  overflow: hidden;
+  border-radius: 16px;
 }
 
 .demoElement {
-  height: 4rem;
-  width: 4rem;
-  background-color: var(--c-brand);
-  border-radius: 1rem;
-  border: 4px solid var(--c-brand-light);
+  width: 6rem;
 }
 </style>
