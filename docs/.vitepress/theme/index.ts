02bfbc3ccad3834dd120edc10f4615d63a9a9013
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-16 19:14:34 +0100

    :pencil: (person) add person to imported components

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,7 @@ import DefaultTheme from 'vitepress/dist/client/theme-default'
 import Features from '../components/Features.vue'
 import Hero from '../components/Hero.vue'
 import Layout from '../components/Layout.vue'
+import Person from '../components/Person.vue'
 import PresetsViewer from '../components/PresetsViewer.vue'
 
 export default {
@@ -17,5 +18,7 @@ export default {
     app.component('PresetsViewer', PresetsViewer)
 
     app.component('Hero', Hero)
+
+    app.component('Person', Person)
   },
 }
