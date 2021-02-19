38cc6fb7bbf28920a2d10b5423cb0f1dc7be5d27
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-19 14:11:22 +0100

    :sparkles: (demo) implement delay demo

---
+++ demo/src/demos/Delay.vue
@@ -0,0 +1,82 @@
+<template>
+  <DemoBox :code-text="codeText">
+    <template #demoElement>
+      <div
+        v-motion
+        :initial="{
+          y: 400,
+        }"
+        :enter="{
+          y: 0,
+          transition: {
+            repeat: Infinity,
+            repeatType: 'mirror',
+            repeatDelay: 1000,
+          },
+        }"
+        class="w-24 h-24 bg-indigo-500 border-8 border-indigo-400 shadow-xl cursor-pointer rounded-3xl"
+      />
+
+      <div
+        v-motion
+        :initial="{
+          y: 400,
+        }"
+        :enter="{
+          y: 0,
+          transition: {
+            repeat: Infinity,
+            repeatType: 'mirror',
+            repeatDelay: 1000,
+          },
+        }"
+        :delay="100"
+        class="w-24 h-24 ml-4 bg-indigo-500 border-8 border-indigo-400 shadow-xl cursor-pointer rounded-3xl"
+      />
+
+      <div
+        v-motion
+        :initial="{
+          y: 400,
+        }"
+        :enter="{
+          y: 0,
+          transition: {
+            repeat: Infinity,
+            repeatType: 'mirror',
+            repeatDelay: 1000,
+          },
+        }"
+        :delay="200"
+        class="w-24 h-24 ml-4 bg-indigo-500 border-8 border-indigo-400 shadow-xl cursor-pointer rounded-3xl"
+      />
+
+      <div
+        v-motion
+        :initial="{
+          y: 400,
+        }"
+        :enter="{
+          y: 0,
+          transition: {
+            repeat: Infinity,
+            repeatType: 'mirror',
+            repeatDelay: 1000,
+          },
+        }"
+        :delay="300"
+        class="w-24 h-24 ml-4 bg-indigo-500 border-8 border-indigo-400 shadow-xl cursor-pointer rounded-3xl"
+      />
+    </template>
+  </DemoBox>
+</template>
+
+<script setup="props" lang="ts">
+import { computed } from 'vue';
+import DemoBox from '../components/DemoBox.vue'
+import delay from '../examples/delay'
+
+const codeText = computed(() => {
+    return delay()
+})
+</script>
