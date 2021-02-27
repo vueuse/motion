8be46ef6b3d8de4aa48eed986eb4b02a08b1b3e5
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-27 01:09:37 +0100

    :sparkles: (format) remove ?.

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -146,5 +146,7 @@ export function getAnimatableNone(key: string, value: string): any {
   let defaultValueType = getValueType(key)
   if (defaultValueType !== filter) defaultValueType = complex
   // If value is not recognised as animatable, ie "none", create an animatable version origin based on the target
-  return defaultValueType.getAnimatableNone?.(value)
+  return defaultValueType.getAnimatableNone
+    ? defaultValueType.getAnimatableNone(value)
+    : undefined
 }
