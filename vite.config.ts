4072b9b7b4a62cb6b69858685ffc611e66d6449d
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-28 20:38:46 +0100

    :wrench: (vite) use windi

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   root: 'demo/',
   optimizeDeps: {
     exclude: ['vue-demi'],
   },
-  plugins: [vue()],
+  plugins: [vue(), WindiCSS()],
   resolve: {
     alias: [
       {
