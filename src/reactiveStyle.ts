3ce9a75d9db940383fbc1ccd4161a85f266a0e12
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-28 16:01:47 +0100

    :sparkles: (reactiveStyle) try to use ref() to improve the Vue 2 support

--- src/reactiveStyle.ts
+++ src/reactiveStyle.ts
@@ -1,4 +1,4 @@
-import { computed, reactive } from 'vue-demi'
+import { reactive, Ref, ref, watch } from 'vue-demi'
 import { StyleProperties } from './types'
 import { getValueAsType, getValueType } from './utils/style'
 
@@ -13,22 +13,31 @@ export function reactiveStyle(props: StyleProperties = {}) {
     ...props,
   })
 
+  const style = ref({}) as Ref<StyleProperties>
+
   // Reactive DOM Element compatible `style` object bound to state
-  const style = computed<StyleProperties>(() => {
-    // Init result object
-    const result: StyleProperties = {}
+  watch(
+    state,
+    () => {
+      // Init result object
+      const result: StyleProperties = {}
 
-    for (const [key, value] of Object.entries(state)) {
-      // Get value type for key
-      const valueType = getValueType(key)
-      // Get value as type for key
-      const valueAsType = getValueAsType(value, valueType)
-      // Append the computed style to result object
-      result[key] = valueAsType
-    }
+      for (const [key, value] of Object.entries(state)) {
+        // Get value type for key
+        const valueType = getValueType(key)
+        // Get value as type for key
+        const valueAsType = getValueAsType(value, valueType)
+        // Append the computed style to result object
+        result[key] = valueAsType
+      }
 
-    return result
-  })
+      style.value = result
+    },
+    {
+      immediate: true,
+      deep: true,
+    },
+  )
 
   return {
     state,
