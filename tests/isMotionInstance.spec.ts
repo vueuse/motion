2226bd578cd9269de2817ee14572d4476a9f64b4
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-06 14:53:34 +0100

    :white_check_mark: (is-motion-instance) test the type guard

---
+++ tests/isMotionInstance.spec.ts
@@ -0,0 +1,33 @@
+import { ref } from '@vue/reactivity'
+import { mount } from '@vue/test-utils'
+import { isMotionInstance, useMotion } from '../src'
+
+const TestComponent = {
+  template: '<div>Hello world</div>',
+}
+
+const getElementRef = () => {
+  const c = mount(TestComponent)
+
+  return ref<HTMLElement>(c.element as HTMLElement)
+}
+describe('isMotionInstance', () => {
+  it('recognize a motion instance', () => {
+    const ref = getElementRef()
+
+    const motionInstance = useMotion(ref)
+
+    expect(isMotionInstance(motionInstance)).toBe(true)
+  })
+
+  it('does not recognize wrong object', () => {
+    const motionInstance = {
+      set: 'test',
+      stopTransitions: {},
+      target: '',
+      apply: 25,
+    }
+
+    expect(isMotionInstance(motionInstance)).toBe(false)
+  })
+})
