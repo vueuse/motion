485bf4fb4e8e67d05792b4a6002d488a4f4fa3c6
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-06 14:53:03 +0100

    :sparkles: (is-motion-instance) add a type guard for motion instance

---
+++ src/utils/is-motion-instance.ts
@@ -0,0 +1,26 @@
+import { isFunction } from '@vueuse/shared'
+import { isRef } from 'vue-demi'
+import { MotionInstance } from '../types'
+
+/**
+ * Check whether an object is a Motion Instance or not.
+ *
+ * Can be useful while building packages based on @vueuse/motion.
+ *
+ * @param obj
+ * @returns bool
+ */
+export function isMotionInstance(obj: any): obj is MotionInstance {
+  const _obj = obj as MotionInstance
+
+  return (
+    _obj.apply !== undefined &&
+    isFunction(_obj.apply) &&
+    _obj.set !== undefined &&
+    isFunction(_obj.set) &&
+    _obj.stopTransitions !== undefined &&
+    isFunction(_obj.stopTransitions) &&
+    _obj.target !== undefined &&
+    isRef(_obj.target)
+  )
+}
