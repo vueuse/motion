0ba95ec022516a981312a7c9083c6faa66c669d2
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-13 23:07:05 +0100

    :sparkles: (demo) add sandbox component

---
+++ demo/src/demos/Sandbox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="h-200 w-full border-4 rounded-4xl border-red-400 flex items-center justify-center"
+  >
+    <Block ref="block" style="transform: perspective(500px)" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from '@vue/reactivity'
+import { useMotion } from '@vueuse/motion'
+
+const block = ref()
+
+const instance = useMotion(block, {
+  initial: {
+    y: 200,
+    opacity: 0,
+  },
+  enter: {
+    y: 0,
+    opacity: 1,
+  },
+})
+</script>
