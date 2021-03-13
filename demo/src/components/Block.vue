e2c4386c7974a5878b09ec4f71c13791f8b446a6
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-13 23:06:55 +0100

    :sparkles: (demo) add block component

---
+++ demo/src/components/Block.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-32 h-32 bg-red-400 border-8 rounded-4xl border-red-600">
+    <slot />
+  </div>
+</template>
--- demo/src/main.ts
+++ demo/src/main.ts
@@ -3,10 +3,13 @@ import 'prism-theme-vars/base.css'
 import { createApp } from 'vue-demi'
 import 'windi.css'
 import App from './App.vue'
+import Block from './components/Block.vue'
 import './index.css'
 
 const app = createApp(App)
 
 app.use(MotionPlugin)
+
+app.component('Block', Block)
 
 app.mount('#app')
