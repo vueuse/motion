b4da578aaab564b8b3d5749548e2435687f22e21
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-14 18:19:54 +0100

    :sparkles: (resolveElement) update resolveElement with better typings

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -14,19 +14,12 @@ export function resolveElement(
     (newVal) => {
       if (!newVal) return
 
-      // We need to resolve the component DOM element
-      // As Vue does not seem to provide an helper for that
-      // But expose the element as `$el` on the Component instance
-      // Just check if it exists and resolve it from there.
-
-      // @ts-ignore
       if (newVal.$el) {
-        // @ts-ignore
         targetRef.value = newVal.$el as MotionTarget
         return
       }
 
-      // $el does not exist, it might then be a
+      // $el does not exist, it might then be a correct DOM element
       targetRef.value = newVal
     },
     { immediate: true },
