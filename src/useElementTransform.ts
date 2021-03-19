93e2b4d21d7bd7802dbe19a9c1932ff8e47e8d68
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-19 22:57:31 +0100

    :sparkles: (useElementTransform) improve initial parsing

--- src/useElementTransform.ts
+++ src/useElementTransform.ts
@@ -1,20 +1,22 @@
 import { MaybeRef, unrefElement } from '@vueuse/core'
-import { set as __set, watch } from 'vue-demi'
+import { watch } from 'vue-demi'
 import { reactiveTransform } from './reactiveTransform'
-import { MotionTarget, PermissiveTarget } from './types'
-import { parseTransform } from './utils/transform-parser'
+import { MotionTarget, PermissiveTarget, TransformProperties } from './types'
+import { stateFromTransform } from './utils/transform-parser'
 
 /**
  * A Composable giving access to a TransformProperties object, and binding the generated transform string to a target.
  *
  * @param target
  */
-export function useElementTransform(target: MaybeRef<PermissiveTarget>) {
+export function useElementTransform(
+  target: MaybeRef<PermissiveTarget>,
+  onInit?: (initData: Partial<TransformProperties>) => void,
+) {
   // Transform cache available before the element is mounted
   let _cache: string | undefined
   // Local target cache as we need to resolve the element from PermissiveTarget
   let _target: MotionTarget = undefined
-
   // Create a reactive transform object
   const { state, transform } = reactiveTransform()
 
@@ -27,18 +29,14 @@ export function useElementTransform(target: MaybeRef<PermissiveTarget>) {
       _target = el
 
       // Parse transform properties and applies them to the current state
-      if (el.style.transform) {
-        Object.entries(parseTransform(el.style.transform)).forEach(
-          ([key, value]) => {
-            __set(state, key, value)
-          },
-        )
-      }
+      if (el.style.transform) stateFromTransform(state, el.style.transform)
 
       // If cache is present, init the target with the current cached value
       if (_cache) {
         el.style.transform = _cache
       }
+
+      if (onInit) onInit(state)
     },
     {
       immediate: true,
@@ -50,7 +48,7 @@ export function useElementTransform(target: MaybeRef<PermissiveTarget>) {
     transform,
     (newValue) => {
       // Add the current value to the cache so it is set on target creation
-      if (!_target || !_target.style) {
+      if (!_target) {
         _cache = newValue
         return
       }
