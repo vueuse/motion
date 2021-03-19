5b7e8080c05dc5685597207c52ceb0ea109177bb
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-19 22:59:16 +0100

    :sparkles: (useMotionProperties) allow default values, retrieve init data from transform & style

--- src/useMotionProperties.ts
+++ src/useMotionProperties.ts
@@ -10,27 +10,51 @@ import { isTransformProp } from './utils/transform'
  *
  * @param target
  */
-export function useMotionProperties(target: MaybeRef<PermissiveTarget>) {
-  // Target element style object
-  const { style } = useElementStyle(target)
-
-  // Target element transform object
-  const { transform } = useElementTransform(target)
-
+export function useMotionProperties(
+  target: MaybeRef<PermissiveTarget>,
+  defaultValues?: Partial<MotionProperties>,
+) {
+  let init = 2
+  // Local motion properties
   const motionProperties = reactive<MotionProperties>({})
 
+  // onInit hook to retrieve parsed data from the target element
+  const onInit = (initData: Partial<MotionProperties>) => {
+    Object.entries(initData).forEach(([key, value]) => {
+      __set(motionProperties, key, value)
+    })
+
+    init--
+  }
+
+  // Target element style object
+  const { style } = useElementStyle(target, onInit)
+
+  // Target element transform object
+  const { transform } = useElementTransform(target, onInit)
+
+  // Apply default values if specified
+  if (defaultValues) {
+    Object.entries(defaultValues).forEach(([key, value]) => {
+      const target = isTransformProp(key) ? transform : style
+
+      __set(target, key, value)
+    })
+  }
+
+  // Watch local object and apply styling accordingly
   watch(
     motionProperties,
     (newVal) => {
-      for (const key in newVal) {
+      if (init) return
+
+      Object.entries(newVal).forEach(([key, value]) => {
         const target = isTransformProp(key) ? transform : style
 
-        if (target[key] && target[key] === newVal[key]) {
-          continue
-        }
+        if (target[key] && target[key] === value) return
 
-        __set(target, key, newVal[key])
-      }
+        __set(target, key, value)
+      })
     },
     {
       immediate: true,
