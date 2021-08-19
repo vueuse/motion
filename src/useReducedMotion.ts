2e88d777a85869a66e8f717de5c818ba30169d1a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-08-20 01:19:48 +0200

    :sparkles: (useReducedMotion) add useReducedMotion composable

---
+++ src/useReducedMotion.ts
@@ -0,0 +1,13 @@
+import { Ref } from 'vue-demi'
+import { useMediaQuery } from '@vueuse/core'
+
+/**
+ * Reactive prefers-reduced-motion.
+ *
+ * @param options
+ */
+export function useReducedMotion(
+  options: { window?: Window } = {},
+): Ref<boolean> {
+  return useMediaQuery('(prefers-reduced-motion: reduce)', options)
+}
