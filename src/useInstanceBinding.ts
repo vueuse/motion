a035765cad8e281a6f0438bc47a562c72f4e57a9
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-08-20 01:18:41 +0200

    :fire: (useInstanceBindings) remove unnecessary composable

--- src/useInstanceBinding.ts
+++
@@ -1,6 +0,0 @@
-import { motionState } from './features/state'
-import { MotionInstance } from './types'
-
-export function useInstanceBinding(key: string, ref: MotionInstance) {
-  motionState[key] = ref
-}
