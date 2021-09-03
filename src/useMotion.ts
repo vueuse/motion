269d5190eab4aee300a06d7293118a507b88134a
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-09-03 18:07:58 +0200

    :sparkles: (directive) update directive (clean properly; fix memory leak)

--- src/useMotion.ts
+++ src/useMotion.ts
@@ -1,4 +1,5 @@
-import { MaybeRef } from '@vueuse/core'
+import { watch } from 'vue-demi'
+import { MaybeRef, noop, tryOnUnmounted } from '@vueuse/core'
 import {
   MotionInstance,
   MotionVariants,
@@ -25,7 +26,8 @@ export function useMotion<T extends MotionVariants>(
   options?: UseMotionOptions,
 ) {
   // Reactive styling and transform
-  const { motionProperties } = useMotionProperties(target)
+  const { motionProperties, stop: stopMotionProperties } =
+    useMotionProperties(target)
 
   // Variants manager
   const { variant, state } = useMotionVariants<T>(variants)
@@ -41,10 +43,34 @@ export function useMotion<T extends MotionVariants>(
     state,
     motionProperties,
     ...controls,
+    stop: (force: boolean = false) => {},
   }
 
   // Bind features
-  useMotionFeatures(instance, options)
+  const { stop: stopMotionFeatures } = useMotionFeatures(instance, options)
+
+  // Instance cleanup function
+  instance.stop = (force: boolean = false) => {
+    const _stop = () => {
+      instance.stopTransitions()
+      stopMotionProperties()
+      stopMotionFeatures()
+    }
+
+    // Check if leave variant exist, if so wait for the animation to end before cleaning up
+    if (!force && variants.value && variants.value['leave']) {
+      const _stopWatch = watch(instance.isAnimating, (newVal) => {
+        if (!newVal) {
+          _stopWatch()
+          _stop()
+        }
+      })
+    } else {
+      _stop()
+    }
+  }
+
+  tryOnUnmounted(() => instance.stop())
 
   return instance
 }
