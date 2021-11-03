c281c9e3838634f724d7fab870041908750751b9
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-11-03 02:19:46 +0100

    :wrench: (build) update build config

--- build.config.ts
+++ build.config.ts
@@ -2,6 +2,7 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
+  inlineDependencies: true,
   entries: [{ input: 'src/index.ts', outDir: 'dist', name: 'index' }],
   externals: ['csstype', '@vueuse/shared', 'framesync', 'style-value-types'],
 })
