a9f22ce34a7797c53595836f4d5f289610d877c3
Yaël Guilloux <yael.guilloux@gmail.com>
2023-10-24 14:34:27 +0200

    chore(docs): update meta & banner

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: '@vueuse/motion',
+    description: '🤹 Vue Composables putting your components in motion',
+    image: 'https://motion.vueuse.org/banner.png',
     socials: {
       twitter: 'yaeeelglx',
       github: 'vueuse/motion',
