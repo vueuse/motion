093925a138b1d79d0790ed7ae2a5575b19d846e5
Bobbie Goede <bobbiegoede@gmail.com>
2024-06-01 11:54:53 +0200

    fix: linting errors

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,7 @@
-import { resolve } from "node:path";
+import { resolve } from 'node:path'
 
 export default defineNuxtConfig({
-  extends: ["@nuxt/ui-pro"],
+  extends: ['@nuxt/ui-pro'],
   features: {
     devLogs: false,
   },
@@ -9,14 +9,14 @@ export default defineNuxtConfig({
     includeWorkspace: true,
   },
   devtools: { enabled: false },
-  routeRules: { "/api/search.json": { prerender: true } },
+  routeRules: { '/api/search.json': { prerender: true } },
   vite: {
     $client: {
       build: {
         rollupOptions: {
           output: {
-            chunkFileNames: "_nuxt/[name]-[hash].js",
-            entryFileNames: "_nuxt/[name]-[hash].js",
+            chunkFileNames: '_nuxt/[name]-[hash].js',
+            entryFileNames: '_nuxt/[name]-[hash].js',
           },
         },
       },
@@ -24,31 +24,31 @@ export default defineNuxtConfig({
   },
 
   alias: {
-    "@vueuse/motion": resolve(__dirname, "../src/index.ts"),
-    "@vueuse/motion/nuxt": resolve(__dirname, "../src/nuxt/module.ts"),
+    '@vueuse/motion': resolve(__dirname, '../src/index.ts'),
+    '@vueuse/motion/nuxt': resolve(__dirname, '../src/nuxt/module.ts'),
   },
   modules: [
-    "@vueuse/motion/nuxt",
-    "@nuxt/content",
-    "@nuxt/ui",
-    "@nuxtjs/google-fonts",
-    "nuxt-og-image",
-    "@nuxt/image",
+    '@vueuse/motion/nuxt',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxtjs/google-fonts',
+    'nuxt-og-image',
+    '@nuxt/image',
   ],
 
   ui: {
-    icons: ["simple-icons"],
+    icons: ['simple-icons'],
   },
 
-  uiPro: { license: "oss" }, // special license for nuxt & nuxt-modules orgs
+  uiPro: { license: 'oss' }, // special license for nuxt & nuxt-modules orgs
 
   // Nuxt Content
   content: {},
 
   // Fonts
   googleFonts: {
-    display: "swap",
+    display: 'swap',
     download: true,
-    families: { "DM+Sans": [400, 500, 600, 700] },
+    families: { 'DM+Sans': [400, 500, 600, 700] },
   },
-});
+})
