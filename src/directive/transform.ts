44d03d4bd47154bb92382e6dabab8cbdd8a34e36
Bobbie Goede <bobbiegoede@gmail.com>
2024-06-03 20:33:38 +0200

    refactor: simplify directive transform

--- src/directive/transform.ts
+++ src/directive/transform.ts
@@ -1,26 +1,20 @@
 import {
-  NodeTypes,
   createArrayExpression,
+  createCallExpression,
   createObjectExpression,
   createObjectProperty,
   createSimpleExpression,
   findProp,
-  isText,
 } from '@vue/compiler-core'
 import type {
-  CompoundExpressionNode,
   DirectiveNode,
   DirectiveTransform,
   ElementNode,
-  InterpolationNode,
   Property,
-  SimpleExpressionNode,
-  TextNode,
   TransformContext,
 } from '@vue/compiler-core'
 import { variantToStyle } from '../utils/transform'
 import * as presets from '../presets'
-import { evaluateValue } from './transpile'
 
 /**
  * Transforms elements that use the `v-motion` directive for SSR support.
@@ -43,7 +37,7 @@ export const directiveTransform: DirectiveTransform = function (
     return result
   }
 
-  // console.log(dir.name.startsWith('motion-'))
+  // Retrieve preset variant style preset directive is used (pattern: motion-[PRESET_KEBAB_CASE])
   let preset: Record<string, any> | undefined
   if (dir.name.startsWith('motion-')) {
     const parts = dir.name.split('motion-').at(1)?.split('-')
@@ -67,84 +61,9 @@ export const directiveTransform: DirectiveTransform = function (
     preset,
   )
 
-  const styleProp = createObjectProperty('style', styleExpression)
-
   return {
-    props: [styleProp],
+    props: [createObjectProperty('style', styleExpression)],
     needRuntime: true,
-  }
-}
-
-const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
-
-const isString = (val: unknown): val is string => typeof val === 'string'
-
-// NodeTypes.DIRECTIVE
-// function isBindDirective(prop: any): prop is DirectiveNode {
-//   return prop != null && prop.type === 7 && prop.name === 'bind'
-// }
-
-// NodeTypes.SIMPLE_EXPRESSION
-function isSimpleExpressionNode(node: any): node is SimpleExpressionNode {
-  return node != null && node.type === NodeTypes.SIMPLE_EXPRESSION
-}
-
-// NodeTypes.COMPOUND_EXPRESSION
-function isCompoundExpressionNode(node: any): node is CompoundExpressionNode {
-  return node != null && node.type === NodeTypes.COMPOUND_EXPRESSION
-}
-
-function mapNodeContentHandler(
-  value:
-    | string
-    | symbol
-    | SimpleExpressionNode
-    | CompoundExpressionNode
-    | TextNode
-    | InterpolationNode,
-): string {
-  if (isString(value)) {
-    return value
-  }
-  else if (isSymbol(value)) {
-    return value.description || ''
-  }
-  else if (isSimpleExpressionNode(value)) {
-    return value.content
-  }
-  else if (isCompoundExpressionNode(value)) {
-    return value.children.map(mapNodeContentHandler).join('')
-  }
-  else if (isText(value)) {
-    if (isString(value.content)) {
-      return value.content
-    }
-    else if (isSimpleExpressionNode(value.content)) {
-      return value.content.content
-    }
-    else if (isCompoundExpressionNode(value.content)) {
-      return value.content.children.map(mapNodeContentHandler).join('')
-    }
-    else {
-      return ''
-    }
-  }
-  else {
-    return ''
-  }
-}
-
-function isConstant(node: SimpleExpressionNode): boolean {
-  if ('isConstant' in node) {
-    // For v3.0.3 earlier
-    return (node as any).isConstant
-  }
-  else if ('constType' in node) {
-    // For v3.0.3 or later
-    return (node.constType as number) > 0
-  }
-  else {
-    throw new Error('Unexpected error while transforming a v-motion directive.')
   }
 }
 
@@ -154,81 +73,27 @@ function createStyleObjectExpressionFromDirectiveNode(
   context: TransformContext,
   preset?: Record<string, any>,
 ): Property['value'] {
-  const properties: Property[] = []
-  // const presetProperties: Property[] = []
   const presetSerialized = JSON.stringify(preset)
   const prefix = context.prefixIdentifiers ? '_ctx.' : ''
   const variantToStyleFn = `${prefix}__motionVariantToStyle`
 
-  if (isSimpleExpressionNode(prop.exp)) {
-    if (isConstant(prop.exp)) {
-      const { status, value } = evaluateValue(prop.exp.content)
-
-      // Add preset values first
-      // if (preset) {
-      //   for (const [key, val] of Object.entries(preset)) {
-      //     presetProperties.push(
-      //       createObjectProperty(
-      //         key,
-      //         createSimpleExpression(String(val), true, prop.loc, 3),
-      //       ),
-      //     )
-      //   }
-      // }
-
-      if (status === 'ok') {
-        // convert variant to style
-        const processed = variantToStyle(value as any)
-        for (const [key, val] of Object.entries(
-          processed as Record<string, any>,
-        )) {
-          properties.push(
-            createObjectProperty(
-              key,
-              createSimpleExpression(String(val), true, prop.loc, 3),
-            ),
-          )
-        }
-      }
-
-      if (preset) {
-        return createArrayExpression([
-          createSimpleExpression(presetSerialized),
-          createObjectExpression(properties),
-        ])
-      }
-
-      return createObjectExpression(properties, node.loc)
-    }
-
-    if (preset) {
-      return createArrayExpression([
-        createSimpleExpression(presetSerialized),
-        createSimpleExpression(
-          `${variantToStyleFn}(${prop.exp.content})`,
-          false,
-        ),
-      ])
-    }
-
-    return createSimpleExpression(
-      `${variantToStyleFn}(${prop.exp.content})`,
-      false,
-    )
+  if (prop.exp == null) {
+    return createObjectExpression([], node.loc)
   }
 
-  if (isCompoundExpressionNode(prop.exp)) {
-    const expression = prop.exp!.children.map(mapNodeContentHandler).join('')
-
-    if (preset) {
-      return createArrayExpression([
-        createSimpleExpression(presetSerialized),
-        createSimpleExpression(`${variantToStyleFn}(${expression})`, false),
-      ])
-    }
-
-    return createSimpleExpression(`${variantToStyleFn}(${expression})`, false)
+  /**
+   * Return array of parsed preset variant style and
+   * call expression that returns parsed variant style from ":initial"
+   */
+  if (preset) {
+    return createArrayExpression([
+      createSimpleExpression(presetSerialized),
+      createCallExpression(variantToStyleFn, [prop.exp]),
+    ])
   }
 
-  return createObjectExpression(properties, node.loc)
+  /**
+   * Return call expression that returns parsed variant style from ":initial"
+   */
+  return createCallExpression(variantToStyleFn, [prop.exp])
 }
