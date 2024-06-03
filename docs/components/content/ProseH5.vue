6b8dad6ab855bad1d8e7fc21a816d2d88a5dbf05
Bobbie Goede <bobbiegoede@gmail.com>
2024-06-03 10:21:17 +0200

    docs: move animation configuration to app config

--- docs/components/content/ProseH5.vue
+++ docs/components/content/ProseH5.vue
@@ -1,8 +1,7 @@
 <template>
   <Motion
     is="h5"
-    :initial="{ y: 100, opacity: 0, transition: { mass: .25, damping: 10 } }"
-    :visible-once="{ y: 0, opacity: 1, transition: { mass: .25, damping: 10 } }"
+    v-bind="$nuxt._appConfig.motions.headers.common"
   >
     <slot />
   </Motion>
