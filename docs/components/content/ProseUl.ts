1b233cd846ade54ff9cd2883fe4fab061a915524
Bobbie Goede <bobbiegoede@gmail.com>
2024-06-03 09:46:54 +0200

    docs: tweaks

--- docs/components/content/ProseUl.ts
+++ docs/components/content/ProseUl.ts
@@ -10,17 +10,22 @@ export default defineComponent({
         'ul',
         {
           // is: 'ul',
-          initial: { y: 100, opacity: 0 },
+          'initial': { y: 100, opacity: 0 },
           'visible-once': { y: 0, opacity: 1 },
         },
         nodes.map((node, i) => {
           node.props ??= {}
           node.props.is = 'li'
           node.props.initial ??= { x: 100, opacity: 0 }
+          node.props.hovered ??= {
+            x: 10,
+            opacity: 1,
+            transition: { mass: 0.5, damping: 5, delay: 0 },
+          }
           node.props['visible-once'] ??= {
             x: 0,
             opacity: 1,
-            transition: { delay: 50 * i },
+            transition: { delay: 50 * i, mass: 0.5, damping: 5 },
           }
 
           // @ts-expect-error type conflict but seems to work fine
