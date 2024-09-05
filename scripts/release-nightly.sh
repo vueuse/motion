f03dab1522c2a3597276effaa36b2e514ad5dd8b
Bobbie Goede <bobbiegoede@gmail.com>
2024-09-05 20:10:36 +0200

    feat: release and nightly release automation

---
+++ scripts/release-nightly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xe
+
+# Restore all git changes
+git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
+
+# Bump versions to edge
+pnpm jiti ./scripts/bump-nightly
+
+# Resolve pnpm
+pnpm i --frozen-lockfile=false
+
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release package
+echo "⚡ Publishing nightly version"
+npx npm@8.17.0 publish --access public --tolerate-republish
