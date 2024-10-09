5f6e3ce3952d0bd23d165afcb36a6ae3cb281978
Bobbie Goede <bobbiegoede@gmail.com>
2024-10-09 10:00:39 +0200

    fix: dependency conflicts

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,36 +2,43 @@ import { resolve } from 'node:path'
 
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
+
   features: {
     devLogs: false,
   },
+
   typescript: {
     includeWorkspace: true,
   },
+
   devtools: { enabled: false },
+
   nitro: {
     devProxy: {
       host: '127.0.0.1',
     },
   },
+
   routeRules: { '/api/search.json': { prerender: true } },
-  vite: {
-    $client: {
-      build: {
-        rollupOptions: {
-          output: {
-            chunkFileNames: '_nuxt/[name]-[hash].js',
-            entryFileNames: '_nuxt/[name]-[hash].js',
-          },
-        },
-      },
-    },
-  },
+
+  // vite: {
+  //   $client: {
+  //     build: {
+  //       rollupOptions: {
+  //         output: {
+  //           chunkFileNames: '_nuxt/[name]-[hash].js',
+  //           entryFileNames: '_nuxt/[name]-[hash].js',
+  //         },
+  //       },
+  //     },
+  //   },
+  // },
 
   alias: {
     '@vueuse/motion': resolve(__dirname, '../src/index.ts'),
-    '@vueuse/motion/nuxt': resolve(__dirname, '../src/nuxt/module.ts'),
+    '@vueuse/motion/nuxt': resolve(__dirname, '../src/nuxt/src/module.ts'),
   },
+
   modules: [
     '@vueuse/motion/nuxt',
     '@nuxt/content',
@@ -45,7 +52,8 @@ export default defineNuxtConfig({
     icons: ['simple-icons'],
   },
 
-  uiPro: { license: 'oss' }, // special license for nuxt & nuxt-modules orgs
+  // special license for nuxt & nuxt-modules orgs
+  uiPro: { license: 'oss' },
 
   // Nuxt Content
   content: {},
@@ -56,4 +64,6 @@ export default defineNuxtConfig({
     download: true,
     families: { 'DM+Sans': [400, 500, 600, 700] },
   },
+
+  compatibilityDate: '2024-10-09',
 })
