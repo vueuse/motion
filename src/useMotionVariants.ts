c995559023e5adb46ea38afcf999e7d71fb54815
Yaël Guilloux <yael.guilloux@gmail.com>
2025-03-10 00:43:16 +0100

    feat useMotionVariants.ts ; add type hint

--- src/useMotionVariants.ts
+++ src/useMotionVariants.ts
@@ -8,7 +8,7 @@ import type { MotionVariants, Variant } from './types'
  */
 export function useMotionVariants<T extends string, V extends MotionVariants<T>>(variants: MaybeRef<V> = {} as MaybeRef<V>) {
   // Unref variants
-  const _variants = unref(variants)
+  const _variants = unref(variants) as V
 
   // Current variant string
   const variant = ref() as Ref<keyof V>
