a1b1ac834fda5f15489d0a9be7fb87215d07ec23
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-10 11:19:15 +0100

    chore: remove unused script

--- scripts/fix-nuxt-build.mjs
+++
@@ -1,8 +0,0 @@
-import { readFile, writeFile } from 'node:fs/promises'
-
-async function patchNuxtCJS() {
-  const content = await readFile('dist/nuxt.cjs', 'utf-8')
-  await writeFile('dist/nuxt.cjs', content.replace('module.exports = module$1;', 'exports.default = module$1;'))
-}
-
-patchNuxtCJS()
